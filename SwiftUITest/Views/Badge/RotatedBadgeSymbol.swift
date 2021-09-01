402e8a29e4daa660f8ea9c02f5697bf0ec0114b4
Vitalii <vlprimary@gmail.com>
2021-09-01 19:26:57 +0300

    Added Badge constructed with GeometryReader

---
+++ SwiftUITest/Views/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUITest
+//
+//  Created by vitalii on 01.09.2021.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+	let angle: Angle
+    var body: some View {
+		BadgeSymbol()
+			.padding(-60)
+			.rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
